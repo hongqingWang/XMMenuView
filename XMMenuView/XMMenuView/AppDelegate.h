cb8bb9b97f4871e9984d7b5183647f12b26f650e
王红庆 <272338444@qq.com>
2019-07-25 14:23:04 +0800

    初始化项目
    初始化项目

---
+++ XMMenuView/XMMenuView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  XMMenuView
+//
+//  Created by JiuFuKeJi on 2019/7/25.
+//  Copyright © 2019 JiuFuKeJi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ XMMenuView/XMMenuView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  XMMenuView
+//
+//  Created by JiuFuKeJi on 2019/7/25.
+//  Copyright © 2019 JiuFuKeJi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
